6a1d9c4805b0b40a3bf9808a382836576422ae33
Azahar Hussain <43411555+azrarn786@users.noreply.github.com>
2021-02-20 14:23:44 +0530

    Update GoodMorning.java

--- GoodMorning.java
+++ GoodMorning.java
@@ -3,6 +3,6 @@ public class GoodMorning
 {   
       public static void main(String[] args)
       {       
-         System.out.println(" Good Morning!");   
+         System.out.println("Good Morning");   
       }
 } 
