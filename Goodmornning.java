8ad6a05c3c2505a02018b9897171a82a0cec573d
Azahar Hussain <43411555+azrarn786@users.noreply.github.com>
2021-02-24 22:18:58 +0530

    Update Goodmornning.java

--- Goodmornning.java
+++ Goodmornning.java
@@ -1,6 +1,6 @@
 public class Goodmorning 
 {
-    public static void main (String args[]) 
+    public static void main (String[] args) 
     {
        System.out.println ("Helloo..Goodmorning!");
     }
